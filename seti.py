d682ec60ae49dce43954684afcce1b38e0d2afd1
matyasi.zsofia <zsofia.matyasi@gmail.com>
2020-12-02 20:42:28 +0100

    decimal_to_base(decimal_number, destination_base) function implemented

--- seti.py
+++ seti.py
@@ -1,10 +1,10 @@
 def decimal_to_binary(decimal_number):
-    binary_list = []
+    destination_base_list = []
     while decimal_number > 0:
         element = decimal_number % 2
-        binary_list.insert(0, element)
+        destination_base_list.insert(0, element)
         decimal_number //= 2
-    return binary_list
+    return destination_base_list
 
 
 def binary_to_decimal(binary_digits):
@@ -14,12 +14,19 @@ def binary_to_decimal(binary_digits):
     return decimal_number
 
 
-print(decimal_to_binary(20))
-print(binary_to_decimal([1, 0, 1, 0, 0]))
 
 def decimal_to_base(decimal_number, destination_base):
-    """Returns the digits in destination_base representation of the decimal number"""
-    pass
+    destination_base_list = []
+    while decimal_number > 0:
+        element = decimal_number % destination_base
+        destination_base_list.insert(0, element)
+        decimal_number //= destination_base
+    return destination_base_list
+
+
+print(decimal_to_binary(20))
+print(binary_to_decimal([1, 0, 1, 0, 0]))
+print(decimal_to_base(20, 8))
 
 
 def base_to_decimal(digits, original_base):
